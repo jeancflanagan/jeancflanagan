132d8b3979240a7a3189891641735cc736f3831a
Oliver Pattison <oliverpattison@gmail.com>
2013-08-14 00:21:22 -0400

    figure.rb: fixed regex; breaks softer; figcaption markdownified

--- _plugins/figure.rb
+++ _plugins/figure.rb
@@ -3,23 +3,38 @@ module Jekyll
 
     def initialize(tag_name, markup, tokens)
       super
+      @align = nil
+      @src = ''
+      @alt = ''
+      @caption = nil
 
       if 
-        markup =~ /(page\.image\[\d\])\s+(page\.image_alt\[\d\])\s+(page\.image_caption\[\d\])\s+(right|left)?/i
-        @src = "{{ site.image_url }}{{ #{$1} }}"
-        @alt = "{{ #{$2} }}"
-        @caption = "{{ #{$3} | markdownify }}"
-        @align = $4
+        markup =~ /(right\s|left\s+)?(page.image\[\d\])(\s+page.image_alt\[\d\])?(\s+page.image_caption\[\d\])?/
+        @align = $1
+        @src = "{{ site.image_url }}{{ #{$2} }}"
+        @alt = "{{ #{$3} }}"
+        @caption = "{{ #{$4} | markdownify }}"
       end
     end
 
     def render(context)
-
       @src = Liquid::Template.parse(@src).render(context)
       @alt = Liquid::Template.parse(@alt).render(context)
       @caption = Liquid::Template.parse(@caption).render(context)
 
-      "<figure class=\"#{@align}\"><img src=\"#{@src}\" alt=\"alt\"/><figcaption>#{@caption}</figcaption></figure>"
+      if @align
+        figure = "<figure class=\"#{@align}\">"
+      else
+        figure = "<figure>"
+      end
+
+      figure += "<img src=\"#{@src}\" alt=\"#{@alt}\"/>"
+      
+      if @caption 
+        figure += "<figcaption>#{@caption}</figcaption>"
+      end
+      
+      figure += "</figure>"
 
     end
   end
