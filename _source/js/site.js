13b645470e48b864c9aeab55d7b9643766ca384f
Oliver Pattison <oliverpattison@gmail.com>
2016-09-30 07:30:49 -0400

    remove and add TODO items

--- _source/js/site.js
+++ _source/js/site.js
@@ -1,9 +1,3 @@
-// TODO add JavaScript here.
-// Other JavaScript files can be added with a `require` statement which browserify will include.
-// Example for Velocity follows:
-//
-// var Velocity = require('../../node_modules/velocity-animate/velocity.min.js');
-
 /* forEach loop through for querySelectorAll
 we need to convert it to an array first */
 var forEach = function (array, callback, scope) {
